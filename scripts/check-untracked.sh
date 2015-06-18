ea291180462cc31f50b5069eba854e790a803e40
David Garcia <dgarcia@parc.com>
2015-06-18 13:50:09 -0700

    added pre-push check for forgotten 'git add' files

---
+++ scripts/check-untracked.sh
@@ -0,0 +1,14 @@
+files=$(git status -u --porcelain --no-column | grep "^??")
+if [ -z "$files" ]; then
+  exit 0
+fi
+
+echo
+echo "ERROR: Preventing push with untracked source files:"
+echo
+echo "$files" | sed "s/^/    /"
+echo
+echo "Either include these files in your commits, add them to .gitignore"
+echo "or stash them with git stash -u."
+echo
+exit 1
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,7 @@
-echo 'copying pre-push git hook into .git/hooks/'
-echo 'python manage.py test' > .git/hooks/pre-push
+echo 'copying pre-push git hooks into .git/hooks/ ...'
+
+echo './scripts/check-untracked.sh || exit 1' > .git/hooks/pre-push
+echo 'python manage.py test' >> .git/hooks/pre-push
 chmod u+x .git/hooks/pre-push
+
+echo 'done'
