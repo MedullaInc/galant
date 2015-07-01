5379ab51157f174aac80d2b4adfb41e1118c35d2
David Garcia <dgarcia@parc.com>
2015-06-30 17:03:32 -0700

    switched to ClientForm

--- gallant/forms.py
+++ gallant/forms.py
@@ -1,1 +1,8 @@
 from django import forms
+from gallant.models import *
+
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ["name", "type", "size", "status", "language", "currency", "notes"]
--- gallant/views.py
+++ gallant/views.py
@@ -1,7 +1,7 @@
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from gallant.models import *
+from django.views.generic.edit import CreateView
+from gallant.forms import *
 
 
 class ClientCreate(CreateView):
-    model = Client
-    fields = ["name", "type", "size", "status", "language", "currency", "notes"]
+    form_class = ClientForm
+    template_name = "gallant/client_form.html"
