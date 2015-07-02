f168c5fe14235d04ebc1a2b4e2a828d0d393acdd
David Garcia <dgarcia@parc.com>
2015-07-02 15:08:19 -0700

    added test to ensure pages blocked for non-logged in users

--- functional_tests/test_signed_out.py
+++ functional_tests/test_signed_out.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from django.utils.translation import activate
-from django.core.urlresolvers import reverse
+from selenium.common.exceptions import NoSuchElementException
+from django.utils.translation import activate, get_language
+from django.core.urlresolvers import reverse, get_resolver
+from django.conf import settings
 
 class SignedOutTest(LiveServerTestCase):
     # fixtures = ['functional_tests/fixtures/ft_one_user.json']
@@ -20,6 +22,7 @@ class SignedOutTest(LiveServerTestCase):
         self.browser.quit()
 
     def test_can_login_int(self):
+        language = get_language()
         for lang, e_text in [('en', u'Account Login'),
                                 ('es', u'Iniciar Sesión')]:
             activate(lang)
@@ -27,10 +30,42 @@ class SignedOutTest(LiveServerTestCase):
             e = self.browser.find_element_by_css_selector(".login-title")
             self.assertEqual(e.text, e_text)
 
+        activate(language)
+
     def test_can_signup_int(self):
+        language = get_language()
         for lang, e_text in [('en', u'Sign Up Now!'),
                                 ('es', u'Regístrate!')]:
             activate(lang)
             self.browser.get(self.live_server_url + reverse('home'))
             e = self.browser.find_element_by_name("signup")
             self.assertEqual(e.text, e_text)
+
+        activate(language)
+
+
+    def test_page_blocked(self):
+        for view_name in get_resolver(None).reverse_dict.keys():
+            if hasattr(view_name, '__call__') \
+                    or 'account' in view_name \
+                    or view_name in ['home']:  # add non-logged in permitted views here
+                continue
+
+            # add singe <pk>-requiring views here:
+            if view_name in ['edit_client', 'client_detail']:
+                url = self.live_server_url + reverse(view_name, args=[0])
+            elif view_name == 'add_client_note':
+                url = self.live_server_url + reverse(view_name, kwargs={'client_id': 0})
+            else:
+                url = self.live_server_url + reverse(view_name)
+
+            self.browser.get(url)
+
+            try:
+                h1 = self.browser.find_element_by_tag_name('h1')
+            except NoSuchElementException:
+                self.fail('h1 Element not found, page is not blocked: %s' %
+                          url)
+
+            self.assertIn('Sign In', h1.text)
+            self.assertTrue( self.live_server_url + reverse('account_login') in self.browser.current_url)
--- gallant/urls.py
+++ gallant/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.conf.urls.i18n import i18n_patterns
 from django.views.generic.base import TemplateView
 from gallant import views
@@ -24,10 +25,10 @@ urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^quotes/$', TemplateView.as_view(template_name='quotes/index.html'), name='quotes'),
-    url(r'^briefs/$', 'briefs.views.index', name='briefs'),
-    url(r'^client/add/$', views.ClientCreate.as_view(), name='add_client'),
-    url(r'^client/edit/(?P<pk>[0-9]+)$', views.ClientUpdate.as_view(), name='edit_client'),
-    url(r'^client/(?P<pk>[0-9]+)$', views.ClientDetailView.as_view(), name='client_detail'),
-    url(r'^client/add_note/(?P<client_id>[0-9]+)$', views.add_client_note, name='add_client_note'),
+    url(r'^quotes/$', login_required(TemplateView.as_view(template_name='quotes/index.html')), name='quotes'),
+    # TODO: url(r'^briefs/$', login_required(briefs.views.index), name='briefs'),
+    url(r'^client/add/$', login_required(views.ClientCreate.as_view()), name='add_client'),
+    url(r'^client/edit/(?P<pk>[0-9]+)$', login_required(views.ClientUpdate.as_view()), name='edit_client'),
+    url(r'^client/(?P<pk>[0-9]+)$', login_required(views.ClientDetailView.as_view()), name='client_detail'),
+    url(r'^client/add_note/(?P<client_id>[0-9]+)$', login_required(views.add_client_note), name='add_client_note'),
 )
