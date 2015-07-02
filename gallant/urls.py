d9aa2aa26a1a7e4ae90f0b60ce54a3620366d92f
David Garcia <dgarcia@parc.com>
2015-07-02 13:15:37 -0700

    added client edit view

--- gallant/urls.py
+++ gallant/urls.py
@@ -27,6 +27,7 @@ urlpatterns = i18n_patterns(
     url(r'^quotes/$', TemplateView.as_view(template_name='quotes/index.html'), name='quotes'),
     url(r'^briefs/$', 'briefs.views.index', name='briefs'),
     url(r'^client/add/$', ClientCreate.as_view(), name='add_client'),
+    url(r'^client/edit/(?P<pk>[0-9]+)$', ClientUpdate.as_view(), name='edit_client'),
     url(r'^client/(?P<pk>[0-9]+)$', ClientDetailView.as_view(), name='client_detail'),
     url(r'^client/add_note/(?P<client_id>[0-9]+)$', add_client_note, name='add_client_note'),
 )
--- gallant/views.py
+++ gallant/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -14,6 +14,21 @@ class ClientCreate(CreateView):
         obj = form.save(commit=True)
         user = GallantUser.objects.get(id=self.request.user.id)
         text = '[Created]\n' + form.cleaned_data['notes']
+        note = Note.objects.create(text=text, created_by=user)
+        obj.notes.add(note)
+        obj.save()
+        return HttpResponseRedirect(reverse('client_detail', args=[obj.id]))
+
+
+class ClientUpdate(UpdateView):
+    model = Client
+    form_class = ClientForm
+    template_name = "gallant/client_form.html"
+
+    def form_valid(self, form):
+        obj = form.save(commit=True)
+        user = GallantUser.objects.get(id=self.request.user.id)
+        text = '[Updated]\n' + form.cleaned_data['notes']
         note = Note.objects.create(text=text, created_by=user)
         obj.notes.add(note)
         obj.save()
