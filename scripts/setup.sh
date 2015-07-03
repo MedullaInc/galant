ba9b385bb92963466954de67c773d1efc8148894
David Garcia <dgarcia@parc.com>
2015-07-03 13:39:12 -0700

    bugfix, python script not bash

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@ echo 'copying pre-push git hooks into .git/hooks/ ...'
 
 echo './scripts/check-untracked.sh || exit 1' > .git/hooks/pre-push
 echo './scripts/test-and-coverage.sh || exit 1' > .git/hooks/pre-push
-echo './scripts/measure-coverage.sh || exit 1' > .git/hooks/pre-push
+echo 'python ./scripts/measure-coverage.py || exit 1' > .git/hooks/pre-push
 chmod u+x .git/hooks/pre-push
 
 echo 'done'
