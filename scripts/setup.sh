654edf2f6e4e1ea72cde51572e6562208266e7f6
David Garcia <dgarcia@parc.com>
2015-07-03 13:41:09 -0700

    bugfix concat

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 echo 'copying pre-push git hooks into .git/hooks/ ...'
 
 echo './scripts/check-untracked.sh || exit 1' > .git/hooks/pre-push
-echo './scripts/test-and-coverage.sh || exit 1' > .git/hooks/pre-push
-echo 'python ./scripts/measure-coverage.py || exit 1' > .git/hooks/pre-push
+echo './scripts/test-and-coverage.sh || exit 1' >> .git/hooks/pre-push
+echo 'python ./scripts/measure-coverage.py || exit 1' >> .git/hooks/pre-push
 chmod u+x .git/hooks/pre-push
 
 echo 'done'
