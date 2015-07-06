9f6294fddd0a16b798f84dd8d795ec8cf6a44c62
Gustavo <gustavillo@gmail.com>
2015-07-06 18:04:34 -0500

    Brief models + view for testing choices

--- gallant/urls.py
+++ gallant/urls.py
@@ -37,5 +37,6 @@ urlpatterns = i18n_patterns(
     url(r'^client/edit/(?P<pk>[0-9]+)$', login_required(gallant.views.ClientUpdate.as_view()), name='edit_client'),
     url(r'^client/(?P<pk>[0-9]+)$', login_required(gallant.views.client_detail), name='client_detail'),
     url(r'^service/add/$', login_required(gallant.views.ServiceCreate.as_view()), name='add_service'),
+    url(r'^service/edit/(?P<pk>[0-9]+)$', login_required(gallant.views.ServiceUpdate.as_view()), name='edit_service'),
     url(r'^service/(?P<pk>[0-9]+)$', login_required(gallant.views.service_detail), name='service_detail'),
 )
