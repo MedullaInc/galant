7e8e274cdba2b0e411e12b30de5ccbe84dbce727
Gustavo <gustavillo@gmail.com>
2015-07-21 15:11:15 -0500

    testing error

--- functional_tests/test_signed_out.py
+++ functional_tests/test_signed_out.py
@@ -51,9 +51,9 @@ class SignedOutTest(LiveServerTestCase):
                 continue
 
             # add singe <pk>-requiring views here:
-            if view_name in ['edit_client', 'client_detail', 'edit_service', 'service_detail','edit_quote', 'quote_detail', 'edit_quote_template']:
+            if view_name in ['edit_client', 'client_detail', 'edit_service', 'service_detail','edit_quote', 'quote_detail', 'edit_quote_template', 'edit_brief']:
                 url = self.live_server_url + reverse(view_name, args=[0])
-            elif view_name in ['brief_detail','edit_brief']:
+            elif view_name in ['brief_detail']:
                 url = self.live_server_url + reverse(view_name, args=['client', 0])
             else:
                 url = self.live_server_url + reverse(view_name)
