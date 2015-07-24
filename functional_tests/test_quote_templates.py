0c8c31065c4603a4ceeb5340d16fa8ab0e34a2a4
David Garcia <dgarcia@parc.com>
2015-07-24 12:59:17 -0700

    fixed test section names in quote template test

--- functional_tests/test_quote_templates.py
+++ functional_tests/test_quote_templates.py
@@ -14,10 +14,10 @@ class QuoteTemplatesTest(browser.SignedInTest):
         b.get(self.live_server_url + reverse('add_quote_template'))
 
         b.find_element_by_name('name').send_keys('Quote test')
-        b.find_element_by_id('id_intro_title').send_keys('test intro title')
-        b.find_element_by_id('id_intro_text').send_keys('test intro text')
-        b.find_element_by_id('id_margin_title').send_keys('test margin title')
-        b.find_element_by_id('id_margin_text').send_keys('test margin text')
+        b.find_element_by_id('id_-section-0_intro_title').send_keys('test intro title')
+        b.find_element_by_id('id_-section-0_intro_text').send_keys('test intro text')
+        b.find_element_by_id('id_-section-1_margin_title').send_keys('test margin title')
+        b.find_element_by_id('id_-section-1_margin_text').send_keys('test margin text')
 
         self._submit_and_check(b)
 
@@ -38,13 +38,13 @@ class QuoteTemplatesTest(browser.SignedInTest):
         b.get(self.live_server_url + reverse('edit_quote_template', args=[qt.id]))
         self.load_scripts()
 
-        b.find_element_by_id('id_intro_title').clear()
-        b.find_element_by_id('id_intro_title').send_keys('modified intro title')
+        b.find_element_by_id('id_-section-0_intro_title').clear()
+        b.find_element_by_id('id_-section-0_intro_title').send_keys('modified intro title')
 
         self._submit_and_check(b)
         self.load_scripts()
 
-        intro = b.find_element_by_id('id_intro_title')
+        intro = b.find_element_by_id('id_-section-0_intro_title')
         self.assertEqual(intro.get_attribute('value'), 'modified intro title')
 
     def test_edit_quote_lang_dropdown(self):
@@ -66,19 +66,19 @@ class QuoteTemplatesTest(browser.SignedInTest):
         self._submit_and_check(b)
 
     def _add_language_and_text(self, b):
-        b.find_element_by_id('id_intro_title').clear()
-        b.find_element_by_id('id_intro_title').send_keys('test intro title')
+        b.find_element_by_id('id_-section-0_intro_title').clear()
+        b.find_element_by_id('id_-section-0_intro_title').send_keys('test intro title')
         b.find_element_by_id('add_translation_button').click()
         b.find_element_by_xpath('//div[@class="popover-content"]//select[@id="id_language"]/option[@value="es"]').click()
         b.find_element_by_xpath('//div[@class="popover-content"]//button').click()
-        b.find_element_by_id('id_intro_title').clear()
-        b.find_element_by_id('id_intro_title').send_keys('titulo de intro prueba')
+        b.find_element_by_id('id_-section-0_intro_title').clear()
+        b.find_element_by_id('id_-section-0_intro_title').send_keys('titulo de intro prueba')
         b.find_element_by_id('en_tab').click()
 
         new_tab = b.find_element_by_xpath('//*[@id="es_tab"]/a')
         self.assertEqual(u'Spanish', new_tab.text)
 
-        intro = b.find_element_by_xpath('//input[@id="id_intro_title"]')
+        intro = b.find_element_by_xpath('//input[@id="id_-section-0_intro_title"]')
         b.find_element_by_id('es_tab').click()
         self.assertEqual(intro.get_attribute('value'), 'titulo de intro prueba')
 
