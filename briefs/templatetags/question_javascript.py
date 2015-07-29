92377f6cc3e29d165f4770422b070e3556336978
David Garcia <dgarcia@parc.com>
2015-07-29 15:24:05 -0700

    added question template files & brief templatetags

---
+++ briefs/templatetags/question_javascript.py
@@ -0,0 +1,23 @@
+from django import template
+from django.template.loader import get_template
+import re
+
+register = template.Library()
+
+
+@register.simple_tag
+def question_form_javascript():
+    """Returns a javascript string to be used with gallant.js's format function
+    """
+    t = get_template('briefs/question_form.html')
+    ret = t.render({'prefix': '{0}', 'extra_class': 'dynamic_section'})
+    return re.sub(r'(.*)', r"'\1' +", ret) + "''"
+
+
+@register.simple_tag
+def multquestion_form_javascript():
+    """Returns a javascript string to be used with gallant.js's format function
+    """
+    t = get_template('briefs/multquestion_form.html')
+    ret = t.render({'prefix': '{0}', 'extra_class': 'dynamic_section'})
+    return re.sub(r'(.*)', r"'\1' +", ret) + "''"
