6c6e60ac6fbf88c5ff9b649ed05d32efb5a361e0
David Garcia <dgarcia@parc.com>
2015-07-29 17:26:49 -0700

    added QuestionForm

--- briefs/forms.py
+++ briefs/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.shortcuts import get_object_or_404
+from django.template.loader import get_template
 from django.utils import six
 from briefs import models as b
 from django.utils.encoding import smart_text
+import re
 
 
 class BriefForm(forms.ModelForm):
@@ -18,6 +21,37 @@ class ClientBriefForm(BriefForm):
     class Meta:
         model = b.ClientBrief
         fields = ['title', 'status']
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = b.Question
+        fields = ['question']
+
+    def __init__(self, data=None, prefix=None, *args, **kwargs):
+        # see if update or create
+        prefix = prefix or ''
+        data = data or {}
+        if prefix + '-id' in data:
+            section = get_object_or_404(q.Section, pk=data[prefix + '-id'])
+            super(QuestionForm, self).__init__(data=data, prefix=prefix, instance=section, *args, **kwargs)
+        else:
+            super(QuestionForm, self).__init__(data=data, prefix=prefix, *args, **kwargs)
+
+    def as_table(self):
+        t = get_template('briefs/question_form.html')
+        if self.instance:
+            question = self.instance
+        else:
+            question = self.save(commit=False)
+        context = {'prefix': self.prefix + '-', 'question': question, 'extra_class': 'dynamic_section'}
+        return t.render(context)
+
+    def save(self, commit=True):
+        if self.prefix:
+            idx = re.match('-question-(\d+)', self.prefix).group(1) or 0
+            self.instance.index = idx
+        return super(QuestionForm, self).save(commit)
 
 
 # class ProjectBriefForm(BriefForm):
