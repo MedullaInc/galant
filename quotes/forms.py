37f5d561802d572d35bd14ca76813ce4f459cfea
David Garcia <dgarcia@parc.com>
2015-08-06 00:12:06 -0700

    restricted clients in quoteform

--- quotes/forms.py
+++ quotes/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from quotes import models as q
 from gallant import models as g
 from gallant import forms as gf
@@ -11,6 +12,17 @@ class QuoteForm(gf.UserModelForm):
     class Meta:
         model = q.Quote
         fields = ['name', 'client', 'status']
+
+    def __init__(self, *args, **kwargs):
+        super(QuoteForm, self).__init__(*args, **kwargs)
+        self.fields['client'].queryset = g.Client.objects.get_for(self.user, 'view_client')
+
+    def clean_client(self):
+        client = self.cleaned_data['client']
+        if self.user.has_perm('view_client', client):
+            return client
+        else:
+            raise ValidationError('Invalid client.')
 
 
 class QuoteTemplateForm(QuoteForm):
