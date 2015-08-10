3635dff6f3dc55282e557a53b91aeb24ac1844d1
David Garcia <dgarcia@parc.com>
2015-08-09 19:20:21 -0700

    added site_processor context processor to populate site name in templates

--- gallant/utils.py
+++ gallant/utils.py
@@ -2,6 +2,7 @@ from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 from django.http.response import Http404
 from django.db import models as m
+from django.contrib.sites.models import Site
 
 LANG_DICT = dict(settings.LANGUAGES)
 
@@ -18,3 +19,8 @@ def get_one_or_404(user, perm, klass, *args, **kwargs):
 class NoNewUsersAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return False
+
+
+# context processor, creates site object for access in templates
+def site_processor(request):
+    return {'site': Site.objects.get_current()}
