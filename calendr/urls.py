303bb73b0f0dacb8e1aff8acb3bc83543fb49f29
David Garcia <dgarcia@parc.com>
2015-09-16 12:15:06 -0700

    added empty calendar app

---
+++ calendr/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    url(r'^$', login_required(TemplateView.as_view(template_name='calendr/calendr.html')), name='calendr'),
+]
