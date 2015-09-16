320ca36b543ce998788dd5944b5b33cd01d0f978
David Garcia <dgarcia@parc.com>
2015-09-15 21:48:30 -0700

    removed useless timeouts, broken pipe error is back

--- functional_tests/test_signed_out.py
+++ functional_tests/test_signed_out.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
+from unittest.case import skip
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -91,12 +92,10 @@ class SignedOutTest(LiveServerTestCase):
     def test_feedback(self):
         b = self.browser
         b.get(self.live_server_url + reverse('feedback'))
-        time.sleep(1.5)  # prevent "broken pipe" PhantomJS browser errors
 
         b.find_element_by_name('email').send_keys('foo@bar.com')
         b.find_element_by_name('feedback').send_keys('asdfasdfasdsadf')
 
         b.find_element_by_xpath('//button[@type="submit"]').click()
-        time.sleep(1.5)  # prevent "broken pipe" PhantomJS browser errors
 
         self.assertEqual(b.find_element_by_tag_name('body').text, 'Thank you.')
