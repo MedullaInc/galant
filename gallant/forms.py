95f39a49543993253185f11927faee6a6a535b68
Gustavo <gustavillo@gmail.com>
2015-09-25 10:45:16 -0500

    close #147

--- gallant/forms.py
+++ gallant/forms.py
@@ -97,16 +97,19 @@ class ProjectOnlyForm(UserModelForm):
 
         if self.instance.id is not None:
             quote_set_a = self.instance.quote_set.all_for(self.user, 'view_quote')
+            quote_set_b = []
 
-            quote_set_b = q.Quote.objects.all_for(
-                self.user, 'view_quote').annotate(projects_count=Count('projects')).filter(
-                projects_count=0, status=5)
+            if len(quote_set_a) > 0:
+                client = quote_set_a[0].client
+                quote_set_b = q.Quote.objects.all_for(
+                    self.user, 'view_quote').annotate(projects_count=Count('projects')).filter(
+                    projects_count=0, status=5, client_id=client.id)
 
             linked_quotes = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple({'checked': True}),
-                                                            queryset=quote_set_a)
+                                                           queryset=quote_set_a)
 
             available_quotes = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple({'checked': False}),
-                                                            queryset=quote_set_b, required=False)
+                                                              queryset=quote_set_b, required=False)
 
             if len(linked_quotes.queryset) > 0:
                 self.fields['linked_quotes'] = linked_quotes
