fd93673214d6f69dd8f603402770d0e065544aeb
Gustavo <gustavillo@gmail.com>
2015-10-08 11:15:53 -0500

    updated coverage for service api endpoint

--- functional_tests/calendr/test_calendr.py
+++ functional_tests/calendr/test_calendr.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+import autofixture
 from functional_tests import browser
 
 
@@ -9,4 +10,11 @@ def tearDown():
 class CalendrTest(browser.SignedInTest):
     def test_can_access_calendr(self):
         response = self.client.get(self.live_server_url + reverse('calendr'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_access_service_endpoint(self):
+        s = autofixture.create_one('gallant.Service', generate_fk=True,
+                                   field_values={'user': self.user})
+
+        response = self.client.get(self.live_server_url + reverse('api_service_detail', args=[s.id]))
         self.assertEqual(response.status_code, 200)
