a217688bc3883ee8ea6b8cc074fa66a6e520137b
David Garcia <dgarcia@parc.com>
2015-11-18 16:54:00 -0800

    added search for client name, status

--- gallant/serializers/client.py
+++ gallant/serializers/client.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.db.models.query import Prefetch
 from moneyed.classes import Money
 from rest_framework import serializers
 from gallant import models as g
@@ -13,20 +14,22 @@ class ClientSerializer(serializers.ModelSerializer):
 
     def get_money_owed(self, client):
         amt = Money(0.00, client.currency)
-        for q in client.quote_set.all_for(self.context['request'].user):
+        for q in client.quote_set.all_for(self.context['request'].user)\
+                .prefetch_related(Prefetch('payments', to_attr='payments_arr')):
             amt += q.get_total_cost()
-            for p in q.payments.all_for(self.context['request'].user):
+            for p in q.payments_arr:
                 amt -= p.amount
         return {'amount': amt.amount, 'currency': str(amt.currency)}
 
     def get_status(self, client):
         status = [client.get_status_display()]
-        for q in client.quote_set.all_for(self.context['request'].user):
+        for q in client.quote_set.all_for(self.context['request'].user)\
+                .prefetch_related(Prefetch('payments', to_attr='payments_arr')):
             owed = q.get_total_cost()
-            for p in q.payments.all_for(self.context['request'].user):
+            for p in q.payments_arr:
                 owed -= p.amount
             if owed > Money(0, owed.currency) and q.payment_due \
-                    and q.payment_due > datetime.date.now():
+                    and q.payment_due < datetime.date.today():
                 status.append('Overdue')
                 break
 
