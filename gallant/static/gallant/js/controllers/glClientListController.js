54f8c7ffd7d514b2b05a7972fb7f10c3619effce
David Garcia <dgarcia@parc.com>
2015-11-20 09:42:55 -0800

    added update button for client list last contacted

--- gallant/static/gallant/js/controllers/glClientListController.js
+++ gallant/static/gallant/js/controllers/glClientListController.js
@@ -19,5 +19,14 @@ app.controller('glClientListController', ['$scope', '$http', '$window', 'Client'
         $scope.redirect = function(clientID) {
             $window.location.href = $scope.clientDetailURL + clientID;
         }
+
+        $scope.updateLastContacted = function(rowIndex) {
+            var client = $scope.clients[rowIndex];
+            var last_contacted = (new Date()).toISOString();
+            Client.update({id: client.id, last_contacted: last_contacted})
+                .$promise.then(function (response) {
+                client.last_contacted = last_contacted;
+            });
+        }
     }
 ]);
