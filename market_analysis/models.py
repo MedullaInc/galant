ca3839c8b690563d11e0ac722deb573d8e76ab6c
Gustavo <gustavillo@gmail.com>
2015-11-26 19:20:12 -0600

    updated django experiments, will not allow non registered users as enrolled for experiment

--- market_analysis/models.py
+++ market_analysis/models.py
@@ -4,4 +4,4 @@ from django.db import models as m
 class CustomerLead(m.Model):
     name = m.CharField(blank=False, max_length=255)
     email = m.EmailField(unique=True)
-    website = m.URLField()
+    website = m.URLField(blank=True)
