0eed2e468d7fc226bb04f7970ddb7708e95420b0
David Garcia <david.agg@gmail.com>
2015-12-07 15:45:43 -0800

    added mark safe to quote section javascript tags

--- quotes/templatetags/section_javascript.py
+++ quotes/templatetags/section_javascript.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.loader import get_template
+from django.utils.safestring import mark_safe
 from gallant import models as g
 import re
 
@@ -12,7 +13,7 @@ def section_form_javascript():
     """
     t = get_template('quotes/section_form.html')
     ret = t.render({'prefix': '{0}', 'name': '{1}', 'label': '{2}', 'extra_class': 'dynamic_section'})
-    return re.sub(r'(.*)', r"'\1' +", ret) + "''"
+    return mark_safe(re.sub(r'(.*)', r"'\1' +", ret) + "''")
 
 @register.simple_tag
 def service_form_javascript():
@@ -21,4 +22,4 @@ def service_form_javascript():
     t = get_template('quotes/service_section_form.html')
     ret = t.render({'prefix': '{0}', 'name': '{1}', 'label': '{2}', 'extra_class': 'dynamic_section',
                     'type_choices': g.ServiceType.choices()})
-    return re.sub(r'(.*)', r"'\1' +", ret) + "''"
+    return mark_safe(re.sub(r'(.*)', r"'\1' +", ret) + "''")
