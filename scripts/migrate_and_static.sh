f43819a79b516562efd455223072c445dbc70add
David Garcia <david.agg@gmail.com>
2015-12-09 15:51:21 -0800

    added activate to migrate & static script fo running from pycharm

--- scripts/migrate_and_static.sh
+++ scripts/migrate_and_static.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+source ./venv/bin/activate
+
 echo 'running migrate...'
 python manage.py migrate || exit 1
 
-echo 'running gulp tasks'
-gulp || exit 1
+echo 'running gulp static'
+gulp static || exit 1
 
 echo 'collecting static files...'
 python manage.py collectstatic -l --noinput || exit 1
