aa908c659e69090c58566c89e55b3fca78135c47
David Garcia <david.agg@gmail.com>
2015-12-15 09:17:55 -0800

    added gallant JS utility unit tests

--- gallant/static/gallant/js/test.js
+++ gallant/static/gallant/js/test.js
@@ -1,7 +1,27 @@
 'use strict';
 
 describe('Gallant utility script', function () {
-    it('adds format() function to String', function () {
-        expect('foo'.format).toBeDefined();
+    describe('format function', function() {
+        it('substitutes strings for {}', function () {
+            expect('{0}, {1}'.format('a', 'b')).toEqual('a, b');
+        });
+    });
+
+    describe('gallant.humanize function', function() {
+        it('changes underscores to uppercase', function () {
+            expect(window.gallant.humanize('hello_world')).toEqual('Hello World');
+        });
+    });
+
+    describe('gallant.copyElement function', function() {
+        it('copies element val() into destination, based on selector', function () {
+            var source = jQuery('<div><input class="testclass" type="text" value="foobar"></div>');
+            var dest = jQuery('<div><input class="testclass" type="text"></div>');
+
+            expect(source.children(':first').val()).toEqual('foobar');
+
+            window.gallant.copyElement(dest, source, '.testclass');
+            expect(dest.children(':first').val()).toEqual(source.children(':first').val());
+        });
     });
 });
