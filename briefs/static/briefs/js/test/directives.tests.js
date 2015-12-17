0401db86fd6bb5910ead1d9382d4d170237d8dab
David Garcia <david.agg@gmail.com>
2015-12-17 12:50:11 -0800

    added question text / checkbox tests

--- briefs/static/briefs/js/test/directives.tests.js
+++ briefs/static/briefs/js/test/directives.tests.js
@@ -14,14 +14,48 @@ describe('brForm', function () {
         });
 
         $scope = $rootScope.$new();
+        $scope.question = {type: 'TextQuestion'};
+        $scope.language = 'en';
     });
 
     describe('brQuestionForm', function () {
         it('compiles', function () {
-            $rootScope.question = {type: 'MultipleChoiceQuestion'};
             var element = $compile('<div br-question-form question="question"></div>')($scope);
             $scope.$digest();
             expect(element.html().substring(0, 3)).toEqual('<!-');
+        });
+
+        it('sets question text', function () {
+            var element = $compile('<div br-question-form question="question" language="language"></div>')($scope);
+            $scope.$digest();
+
+            var q = angular.element(element.find('input')[1]);
+            q.val('Huh?').triggerHandler('input');
+
+            expect($scope.question.question[$scope.language]).toEqual('Huh?');
+        });
+
+        it('sets multiquestion text', function () {
+            $scope.question.type = 'MultipleChoiceQuestion';
+            var element = $compile('<div br-question-form question="question" language="language"></div>')($scope);
+            $scope.$digest();
+
+            var q = angular.element(element.find('input')[1]);
+            q.val('Huh?').triggerHandler('input');
+
+            expect($scope.question.question[$scope.language]).toEqual('Huh?');
+        });
+
+        it('sets multiquestion select multiple', function () {
+            $scope.question.type = 'MultipleChoiceQuestion';
+            $scope.question.can_select_multiple = false;
+            var element = $compile('<div br-question-form question="question" language="language"></div>')($scope);
+            $scope.$digest();
+
+            var q = angular.element(element.find('input')[0]);
+            q.prop('checked', true).triggerHandler('click');
+
+            expect($scope.question.can_select_multiple).toEqual(true);
         });
     });
 });
