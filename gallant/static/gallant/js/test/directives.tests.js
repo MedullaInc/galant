51915d108347a5661a3bf719265f93afb9dca1fb
David Garcia <david.agg@gmail.com>
2015-12-17 11:40:56 -0800

    added ultext directive tests

--- gallant/static/gallant/js/test/directives.tests.js
+++ gallant/static/gallant/js/test/directives.tests.js
@@ -1,6 +1,7 @@
 describe('glForm', function () {
     var $rootScope;
     var $compile;
+    var $scope;
 
     beforeEach(function () {
         module('gallant.directives.glForm');
@@ -11,13 +12,34 @@ describe('glForm', function () {
             $rootScope = _$rootScope_;
             $compile = _$compile_;
         });
+
+
+        $rootScope.language = 'en';
+        $rootScope.text = {en: 'sadf'}
+        $scope = $rootScope.$new();
     });
 
     describe('glRequiredErrors', function () {
         it('compiles', function () {
-            var element = $compile("<div gl-required-errors></div>")($rootScope);
-            $rootScope.$digest();
-            expect(element.html().substring(0,3)).toEqual('<ul');
+            var element = $compile('<div gl-required-errors></div>')($scope);
+            $scope.$digest();
+            expect(element.html().substring(0, 3)).toEqual('<ul');
+        });
+    });
+
+    describe('glUltextInput', function () {
+        it('compiles', function () {
+            var element = $compile('<div gl-ultext-input></div>')($scope);
+            $scope.$digest();
+            expect(element.html().substring(0, 3)).toEqual('<in');
+        });
+    });
+
+    describe('glUltextArea', function () {
+        it('compiles', function () {
+            var element = $compile('<div gl-ultext-area></div>')($scope);
+            $scope.$digest();
+            expect(element.html().substring(0, 3)).toEqual('<te');
         });
     });
 });
