3883ccb60d96748830489b83532d44ad177625a8
Gustavo Reyes <gusreyes01@gmail.com>
2015-12-19 12:33:28 -0600

    added quotes tests

--- quotes/static/quotes/js/directives/qtForm.js
+++ quotes/static/quotes/js/directives/qtForm.js
@@ -5,169 +5,154 @@ app = angular.module('quotes.directives.qtForm', [
   'as.sortable',
 ]);
 
-app.directive('qtQuoteForm', ['Quote', 'Service','Client', '$filter', function(Quote, Service, Client, $filter) {
-  return {
-    restrict: 'A',
-    scope: {
-      quote: '=',
-      endpoint: '=',
-      language: '=',
-      forms: '=',
-    },
+app.directive('qtQuoteForm', ['Quote', 'Service', 'Client', '$filter', function(Quote, Service, Client, $filter) {
+    return {
+        restrict: 'A',
+        scope: {
+          quote: '=',
+          endpoint: '=',
+          language: '=',
+          forms: '=',
+        },
     controller: ['$scope', '$attrs', '$filter', 'Quote', 'Service', 'QuoteTemplate', 'Client',
-      function($scope, $attrs, $filter, Quote, Service, QuoteTemplate, Client) {
-        $scope.isCollapsed = true;
-        $scope.quoteFields = [];
-        $scope.quoteStatus = [];
-        $scope.serviceFields = [];
+        function($scope, $attrs, $filter, Quote, Service, QuoteTemplate, Client) {
+            $scope.isCollapsed = true;
+            $scope.quoteFields = [];
+            $scope.quoteStatus = [];
+            $scope.serviceFields = [];
 
-        $scope.endpoint = Quote;
+            $scope.endpoint = Quote;
 
-        $scope.addSection = function(section_name) {
-          var name = "";
-          if (section_name) {
-            name = section_name;
-          } else {
-            var counter = $scope.quote.sections.length;
-            name = "section_" + (counter++);
-          }
-          $scope.inserted = {
-            title: "",
-            text: "",
-            name: name,
-            index: counter,
-          }
+            $scope.addSection = function(section_name) {
+                var name = "";
+                if (section_name) {
+                    name = section_name;
+                } else {
+                    var counter = $scope.quote.sections.length;
+                    name = "section_" + (counter++);
+              }
+              $scope.inserted = {
+                  title: "",
+                  text: "",
+                  name: name,
+                  index: counter,
+              }
+              $scope.quote.sections.push($scope.inserted);
+            };
 
-          $scope.quote.sections.push($scope.inserted);
-        };
+            $scope.addService = function() {
+                $scope.inserted = {
+                    cost: {
+                        amount: "0",
+                        currency: "USD",
+                    },
+                    description: "N/A",
+                    user: 1,
+                    name: "",
+                    notes: Array[0],
+                    parent: null,
+                    quantity: "",
+                    type: "",
+                    user: "1",
+              };
+              $scope.quote.services.push($scope.inserted);
+            };
 
-        $scope.addService = function() {
-          $scope.inserted = {
-            cost: {
-              amount: "0",
-              currency: "USD",
-            },
-            description: "N/A",
-            user: 1,
-            name: "",
-            notes: Array[0],
-            parent: null,
-            quantity: "",
-            type: "",
-            user: "1",
-          };
-          $scope.quote.services.push($scope.inserted);
-        };
-
-
-        Client.query({
-        }).$promise.then(function (clients) {
-            $scope.clients = clients;
-        });
-
-
-        Quote.fields({
-        }).$promise.then(function (fields) {
-                for (var key in fields.status) {
-                  // must create a temp object to set the key using a variable
-                  var tempObj = {};
-                  tempObj[key] = fields.status[key];
-                  $scope.quoteStatus.push({value: key, text: tempObj[key]});
-                }
-        });
-
-        Service.fields({}).$promise.then(function(fields) {
-          for (var key in fields.type) {
-            // must create a temp object to set the key using a variable
-            var tempObj = {};
-            tempObj[key] = fields.type[key];
-            $scope.serviceFields.push({
-              value: key,
-              text: tempObj[key]
+            Client.query().$promise.then(function(clients) {
+                $scope.clients = clients;
             });
-          }
-
-        });
-        if ($attrs.quoteId) {
-          Quote.get({
-            id: $attrs.quoteId
-          }).$promise.then(function(quote) {
-            $scope.quote = quote;
-          });
-        } else {
-
-          if ($attrs.quoteTemplateId) {
-            
-
-          } else {
-          $scope.quote = {
-            "id": "",
-            "user": $attrs.userId,
-            "name": "New Quote",
-            "client": $attrs.clientId,
-            "sections": [],
-            "services": [],
-            "status": "0",
-            "modified": "",
-            "token": "",
-            "parent": null,
-            "projects": []
-          }
-          $scope.addSection('intro');
-          $scope.addSection('important_notes');
-          $scope.addService();
-        }
-
-      }
-    }],
-    templateUrl: '/static/quotes/html/qt_quote_form.html',
-    link: function($scope) {
 
 
+            Quote.fields().$promise.then(function(fields) {
+                for (var key in fields.status) {
+                    // must create a temp object to set the key using a variable
+                    var tempObj = {};
+                    tempObj[key] = fields.status[key];
+                    $scope.quoteStatus.push({
+                        value: key,
+                        text: tempObj[key]
+                    });
+                }
+            });
 
-      $scope.removeServiceSection = function(index) {
-        $scope.quote.services.splice(index, 1);
-      };
-
-      $scope.showType = function(service) {
-
-        if (service) {
-          var selected = [];
-          selected = $filter('filter')($scope.serviceFields, {
-            value: service.type
-          });
-          return selected.length ? selected[0].text : 'Not set';
-        }
-      };
-
-      $scope.getTotal = function() {
-        if ($scope.quote) {
-          $scope.total = 0;
-          for (var i = 0; i < $scope.quote.services.length; i++) {
-            var service = $scope.quote.services[i];
-            if (service) {
-              $scope.total += (service.cost.amount * service.quantity);
+            Service.fields({}).$promise.then(function(fields) {
+                for (var key in fields.type) {
+                    // must create a temp object to set the key using a variable
+                    var tempObj = {};
+                    tempObj[key] = fields.type[key];
+                    $scope.serviceFields.push({
+                        value: key,
+                        text: tempObj[key]
+                    });
+                }
+            });
+            if ($attrs.quoteId) {
+                Quote.get({id: $attrs.quoteId}).$promise.then(function(quote) {
+                    $scope.quote = quote;
+                });
             } else {
-              $scope.total += 0;
+                if ($attrs.quoteTemplateId) {
+                
+                } else {
+                    $scope.quote = {
+                        "id": "",
+                        "user": $attrs.userId,
+                        "name": "New Quote",
+                        "client": $attrs.clientId,
+                        "sections": [],
+                        "services": [],
+                        "status": "0",
+                        "modified": "",
+                        "token": "",
+                        "parent": null,
+                        "projects": []
+                    }
+                    $scope.addSection('intro');
+                    $scope.addSection('important_notes');
+                    $scope.addService();
+              }
             }
           }
-          return $scope.total;
-        }
-      }
+        ],
+        templateUrl: '/static/quotes/html/qt_quote_form.html',
+        link: function($scope) {
+            $scope.removeServiceSection = function(index) {
+                $scope.quote.services.splice(index, 1);
+            };
+            $scope.showType = function(service) {
+                if (service) {
+                    var selected = [];
+                    selected = $filter('filter')($scope.serviceFields, {
+                        value: service.type
+                    });
+                    return selected.length ? selected[0].text : 'Not set';
+                }
+            };
+
+            $scope.getTotal = function() {
+                if ($scope.quote) {
+                    $scope.total = 0;
+                    for (var i = 0; i < $scope.quote.services.length; i++) {
+                        var service = $scope.quote.services[i];
+                        if (service) {
+                            $scope.total += (service.cost.amount * service.quantity);
+                        } else {
+                            $scope.total += 0;
+                        }
+                    }
+                return $scope.total;
+                }
+            }
+
+            $scope.removeSection = function(index) {
+                $scope.quote.sections.splice(index, 1);
+            };
 
 
-      $scope.removeSection = function(index) {
-        $scope.quote.sections.splice(index, 1);
-      };
-
-
-      $scope.dragControlListeners = {
-        orderChanged: function(event) {
-
-        },
-      };
-
-
+            $scope.dragControlListeners = {
+                orderChanged: function(event) {
+            },
+          };
     }
   };
 }]);
