3c1335c7526184efbb631a55be82054a21fef2bf
Gustavo Reyes <gusreyes01@gmail.com>
2016-01-21 16:12:36 -0600

    updated quotes functional tests

--- functional_tests/quotes/test_quote_templates.py
+++ functional_tests/quotes/test_quote_templates.py
@@ -11,41 +11,78 @@ def tearDown():
 class QuoteTemplatesTest(browser.SignedInTest):
     def test_add_quote_template(self):
         b = browser.instance()
+
+        q = get_blank_quote_autofixture(self.user)
+        q.save()
+
         b.get(self.live_server_url + reverse('add_quote_template'))
 
-        b.find_element_by_name('name').send_keys('Quote test')
-        b.find_element_by_id('id_-section-0-title').send_keys('test intro title')
-        b.find_element_by_id('id_-section-0-text').send_keys('test intro text')
-        b.find_element_by_id('id_-section-1-title').send_keys('test margin title')
-        b.find_element_by_id('id_-section-1-text').send_keys('test margin text')
-        b.find_element_by_id('id_-service-2-name').send_keys('1234')
-        b.find_element_by_xpath('//select[@name="-service-2-type"]/option[@value="3"]').click()
+
+        b.find_element_by_id('edit_quote').click()
+        b.find_element_by_id('quote_name').send_keys('Quote test')
+        b.find_element_by_xpath('//select[@name="client"]/option[@value="number:1"]').click()
+        b.find_element_by_id('save_quote').click()
+
+        b.find_element_by_id('edit_service_0').click()
+        b.find_element_by_id('service_name_0').send_keys('1234')
+        b.find_element_by_id('quantity_0').send_keys('1')
+        b.find_element_by_xpath('//select[@id="type_0"]/option[@value="string:1"]').click()
+        b.find_element_by_id('save_service_0').click()
+
+        b.find_element_by_id('text_1').send_keys('test intro text')
+        b.find_element_by_id('title_1').send_keys('test margin title')
+        b.find_element_by_id('save_section_1').click()
+
+        #b.find_element_by_xpath('//select[@e-name="-service-2-type"]/option[@value="3"]').click()
 
         self._submit_and_check(b)
 
     def test_add_quote_lang_dropdown(self):
         b = browser.instance()
+
+        c = autofixture.create_one('gallant.Client', generate_fk=True,
+                                   field_values={'user': self.user})
+        c.save()     
+
         b.get(self.live_server_url + reverse('add_quote_template'))
 
-        b.find_element_by_name('name').send_keys('Quote test')
-        b.find_element_by_xpath('//select[@name="-service-2-type"]/option[@value="3"]').click()
+        b.find_element_by_id('edit_service_0').click()
+        b.find_element_by_id('service_name_0').send_keys('1234')
+        b.find_element_by_id('quantity_0').send_keys('1')
+        b.find_element_by_xpath('//select[@id="type_0"]/option[@value="string:1"]').click()
+        b.find_element_by_id('save_service_0').click()
+
+        #b.find_element_by_xpath('//select[@e-name="-service-2-type"]/option[@value="3"]').click()
         self._add_language_and_text(b)
 
         self._submit_and_check(b)
 
-    def test_edit_quote_template(self):
+    def test_quote_template_detail(self):
         b = browser.instance()
         q = get_blank_quote_autofixture(self.user)
         qt = autofixture.create_one('quotes.QuoteTemplate', generate_fk=False,
                                     field_values={'quote': q, 'user': self.user})
-        b.get(self.live_server_url + reverse('edit_quote_template', args=[qt.id]))
+        b.get(self.live_server_url + reverse('quote_template_detail', args=[qt.id]))
 
-        b.find_element_by_id('id_-section-0-title').clear()
-        b.find_element_by_id('id_-section-0-title').send_keys('modified intro title')
+        b.find_element_by_id('edit_quote').click()
+        b.find_element_by_id('quote_name').send_keys('Quote test')
+        b.find_element_by_xpath('//select[@name="client"]/option[@value="number:1"]').click()
+        b.find_element_by_id('save_quote').click()
+        
+        b.find_element_by_id('edit_section_0').click()
+        b.find_element_by_id('title_0').clear()
+        b.find_element_by_id('title_0').send_keys('modified intro title')
+        b.find_element_by_id('text_0').clear()
+        b.find_element_by_id('text_0').send_keys('modified intro title')
+        b.find_element_by_id('save_section_0').click()
+        
 
         self._submit_and_check(b)
+        
 
-        intro = b.find_element_by_id('id_-section-0-title')
+
+        b.find_element_by_id('edit_section_0').click()
+        intro = b.find_element_by_id('title_0')
         self.assertEqual(intro.get_attribute('value'), 'modified intro title')
 
     def test_delete_quote_template(self):
@@ -64,7 +101,13 @@ class QuoteTemplatesTest(browser.SignedInTest):
         q = get_blank_quote_autofixture(self.user)
         qt = autofixture.create_one('quotes.QuoteTemplate', generate_fk=False,
                                     field_values={'quote': q, 'user': self.user})
-        b.get(self.live_server_url + reverse('edit_quote_template', args=[qt.id]))
+
+        c = autofixture.create_one('gallant.Client', generate_fk=True,
+                                   field_values={'user': self.user})
+        c.save()     
+
+        b.get(self.live_server_url + reverse('quote_template_detail', args=[qt.id]))
+
         self._add_language_and_text(b)
 
         self._submit_and_check(b)
@@ -74,48 +117,51 @@ class QuoteTemplatesTest(browser.SignedInTest):
 
         q = get_blank_quote_autofixture(self.user)
         b.get(self.live_server_url + reverse('add_quote_template', kwargs={'quote_id': q.id}))
-
-        intro_title = b.find_element_by_id('id_-section-0-title_hidden')
-        self.assertEqual(q.intro().title.json(), intro_title.get_attribute('value'))
+        b.find_element_by_id('edit_section_0').click()
+        intro_title = b.find_element_by_id('title_0')
+        self.assertEqual(q.intro().title, {})
 
     def test_add_quote_from_template(self):
 
         b = browser.instance()
         q = get_blank_quote_autofixture(self.user)
-        q.intro().title.set_text('en', 'hello')
-        q.intro().save()
         qt = autofixture.create_one('quotes.QuoteTemplate', generate_fk=False,
                                     field_values={'quote': q, 'user': self.user})
         c = autofixture.create_one('gallant.Client', generate_fk=True,
                                    field_values={'user': self.user})
         c.save()
         b.get(self.live_server_url + reverse('add_quote') + '?template_id=%d&lang=en' % qt.id)
+        b.find_element_by_id('edit_section_0').click()
+        intro_title = b.find_element_by_id('title_0')
 
-        intro_title = b.find_element_by_id('id_-section-0-title_hidden')
-        self.assertEqual(q.intro().title.json(), intro_title.get_attribute('value'))
-        b.find_element_by_xpath('//select[@name="client"]/option[@value="%d"]' % c.id).click()
-        b.find_element_by_id('create_submit').click()
-
+        
+        with browser.wait_for_page_load():
+            b.find_element_by_id('create_submit').click()
         success_message = b.find_element_by_class_name('alert-success')
         self.assertTrue(u'Quote saved.' in success_message.text)
 
     def _add_language_and_text(self, b):
-        b.find_element_by_id('id_-section-0-title').clear()
-        b.find_element_by_id('id_-section-0-title').send_keys('test intro title')
+        b.find_element_by_id('edit_section_0').click()
+        b.find_element_by_id('title_0').clear()
+        b.find_element_by_id('title_0').send_keys('test intro title')
         b.find_element_by_id('add_translation_button').click()
-        b.find_element_by_xpath('//div[@class="popover-content"]//select[@id="id_language"]/option[@value="es"]').click()
-        b.find_element_by_xpath('//div[@class="popover-content"]//button').click()
-        b.find_element_by_id('id_-section-0-title').clear()
-        b.find_element_by_id('id_-section-0-title').send_keys('titulo de intro prueba')
-        b.find_element_by_id('en_tab').click()
+        b.find_element_by_xpath('//div[@class="popover-content"]//select[@id="id_language"]/option[@value="string:en"]').click()
+        b.find_element_by_id('language_add').click()
+        b.find_element_by_xpath('//div[@class="popover-content"]//select[@id="id_language"]/option[@value="string:es"]').click()
+        b.find_element_by_id('language_add').click()
+        b.find_element_by_id('es_tab').click()
+        b.find_element_by_id('title_0').clear()
+        b.find_element_by_id('title_0').send_keys('titulo de intro prueba')
+        b.find_element_by_id('save_section_0').click()
 
         self._submit_and_check(b)
 
-        new_tab = b.find_element_by_xpath('//*[@id="es_tab"]/a')
+        new_tab = b.find_element_by_xpath('//*[@id="es_tab"]')
         self.assertEqual(u'Spanish', new_tab.text)
 
-        intro = b.find_element_by_xpath('//input[@id="id_-section-0-title"]')
+        b.find_element_by_id('edit_section_0').click()
         b.find_element_by_id('es_tab').click()
+        intro = b.find_element_by_id('title_0')
         self.assertEqual(intro.get_attribute('value'), 'titulo de intro prueba')
 
     def test_can_access_quote_template_endpoint(self):
@@ -123,18 +169,12 @@ class QuoteTemplatesTest(browser.SignedInTest):
         qt = autofixture.create_one('quotes.QuoteTemplate', generate_fk=False,
                                     field_values={'quote': q, 'user': self.user})
 
-        response = self.client.get(self.live_server_url + reverse('api_quote_template_detail', args=[qt.id]))
+        response = self.client.get(self.live_server_url + reverse('api-quote-template-detail', args=[qt.id]))
         self.assertEqual(response.status_code, 200)
 
     def _submit_and_check(self, b):
-        b.find_element_by_id('create_submit').click()
+        with browser.wait_for_page_load():
+            b.find_element_by_id('create_submit').click()        
         self.disable_popups()
-
         success_message = b.find_element_by_class_name('alert-success')
-        self.assertTrue(u'Quote saved.' in success_message.text)
-
-    def _submit_and_check(self, b):
-        b.find_element_by_id('create_submit').click()
-
-        success_message = b.find_element_by_class_name('alert-success')
-        self.assertTrue(u'Template saved.' in success_message.text)
+        self.assertTrue(u'Quote Template saved.' in success_message.text)
