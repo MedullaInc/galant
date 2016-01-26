2a1bff9f511e385a7e8c7af9fe0fe0c189a9e051
David Garcia <david.agg@gmail.com>
2016-01-25 16:57:15 -0800

    removed nosepicker tests

--- scripts/test-and-coverage.sh
+++ scripts/test-and-coverage.sh
@@ -1,5 +1,4 @@
 echo 'Erasing python coverage data...'
 ./venv/bin/coverage erase
-python ./scripts/test-nose-picker.py || { rm .coverage*local*; exit 1; }
-./venv/bin/coverage combine
+./venv/bin/coverage run manage.py test --noinput || { rm .coverage*local*; exit 1; }
 
