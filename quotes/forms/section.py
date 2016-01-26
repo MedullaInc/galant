1dac13bd604f996a815572185669490114cc2ede
Gustavo Reyes <gusreyes01@gmail.com>
2016-01-26 10:02:05 -0600

    removed forms "section.py" completely

--- quotes/forms/section.py
+++
@@ -1,9 +0,0 @@
-from gallant.utils import get_one_or_404
-from quotes import models as q
-from gallant import models as g
-from gallant import forms as gf
-from django.utils.translation import ugettext_lazy as _
-from django.template.loader import get_template
-
-
-
