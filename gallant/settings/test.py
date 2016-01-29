b6c660db8b1825fa18d6e0fa7e390dbe1e51d4df
David Garcia <david.agg@gmail.com>
2016-01-28 17:32:56 -0800

    tweaks to improve test performance

--- gallant/settings/test.py
+++ gallant/settings/test.py
@@ -1,10 +1,17 @@
 from gallant.settings.base import *
 
-os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = "localhost:8000-8010,8080,9200-9300"
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = "localhost:8001-8010,9200-9300"
 
 DATABASES = {
     'default': {
         'NAME': ':memory:',
         'ENGINE': 'django.db.backends.sqlite3',
     },
+}
+
+MIGRATION_MODULES = {
+    'gallant': None,
+    'quotes': None,
+    'briefs': None,
+    'calendr': None,
 }
