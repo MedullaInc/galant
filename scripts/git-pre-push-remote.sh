7bb96ee821d3ffa412c55438136a4c5d4bd9dfe2
David Garcia <david.agg@gmail.com>
2016-02-02 15:52:13 -0800

    added explanatory echo statements for remote script

--- scripts/git-pre-push-remote.sh
+++ scripts/git-pre-push-remote.sh
@@ -8,5 +8,7 @@ fi
 echo 'Running JS tests / coverage...'
 gulp test || exit 1
 
+echo 'rsyncing to remote git repo...'
 rsync -avz -e ssh .git/ stg:gallant/.git/
+echo 'running remote test / coverage...'
 ssh stg 'cd gallant; ./scripts/test-remote.sh'
