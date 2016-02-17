1b803866e8663fe31e9e771274fb235e3db1e29d
Gustavo Reyes <gusreyes01@gmail.com>
2016-02-16 19:08:07 -0600

    added qtClientForm.js

---
+++ quotes/static/quotes/js/directives/qtClientForm.js
@@ -0,0 +1,39 @@
+app = angular.module('quotes.directives.qtClientForm', [
+    'quotes.services.qtServices',
+    'quotes.filters.qtCutFilter',
+    'quotes.directives.qtServiceTable',
+    'quotes.directives.qtSectionTable',
+    'gallant.directives.glForm',
+    'ui.bootstrap',
+    'as.sortable']);
+
+app.directive('qtClient', ['Quote', function (Quote) {
+    return {
+        restrict: 'A',
+        scope: {
+            quote: '=',
+            language: '=',
+            idType: '=',
+        },
+        controller: ['$scope', '$attrs', '$filter', '$window', 'Quote',
+            function ($scope, $attrs, $filter, $window, Quote) {
+                $scope.idType = $attrs.idType;
+                
+                if($attrs.idType == "token"){
+                    $window.onbeforeunload  = function () { 
+                        $scope.quote.session_duration = ((new Date() - $scope.$parent.startTime)/1000 );
+                        $scope.quote.views  = $scope.quote.views+1;
+                        $scope.quote.status = "3";
+                        Quote.update({id: $scope.quote.id}, $scope.quote);
+                    }
+                }
+
+                Quote.getUser({id: $attrs.quoteId, user: $attrs.userId}).$promise.then(function (quote) {
+                    $scope.quote = quote;
+                });
+
+                }],
+        templateUrl: '/static/quotes/html/qt_quote_form.html',
+        link: function ($scope) {}
+    };
+    }]);
