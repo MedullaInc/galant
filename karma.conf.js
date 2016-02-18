6f2da9fe393526adc3a8991e239f7dc838e31d98
David Garcia <david.agg@gmail.com>
2016-02-18 11:05:30 -0800

    added karma-angular-filesort to prevent frequent phantomjs disconnect (JS import order issue apparently)

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,14 @@ module.exports = function (config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine', 'angular-filesort'],
+
+
+        angularFilesort: {
+          whitelist: [
+            '**/static/**/*.js'
+          ]
+        },
 
 
         // list of files / patterns to load in the browser
