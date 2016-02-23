ce1fd356aa428e65ee50da3cab796cdf97835720
Gustavo Reyes <gusreyes01@gmail.com>
2016-02-23 01:04:14 -0600

    removed leftover function in quotes

--- quotes/views/quote.py
+++ quotes/views/quote.py
@@ -201,23 +201,6 @@ class SectionViewSet(ModelViewSet):
 			return self.model.objects.all_for(self.request.user)
 
 
-class MultipleFieldLookupMixin(object):
-	"""
-	Apply this mixin to any view or viewset to get multiple field filtering
-	based on a `lookup_fields` attribute, instead of the default single field filtering.
-	"""
-	def get_object(self):
-		queryset = self.get_queryset()             # Get the base queryset
-		queryset = self.filter_queryset(queryset)  # Apply any filter backends
-		filter = {}
-		value = self.kwargs[self.lookup_field]
-		for field in self.lookup_fields:
-			try:
-				obj = get_object_or_404(queryset, **{ field: value})  # Lookup the object
-			except:
-				continue
-			return obj
-
 @permission_classes((AllowAny, ))
 class QuoteClientViewSet(UserModelViewSet):
 	model = q.Quote
