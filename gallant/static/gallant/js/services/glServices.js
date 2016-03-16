f1d3f655d33e9a5b2115c50d1ac1a4745576778c
David Garcia <david.agg@gmail.com>
2016-03-16 11:07:46 -0700

    removed superfluous Client resource definition

--- gallant/static/gallant/js/services/glServices.js
+++ gallant/static/gallant/js/services/glServices.js
@@ -2,45 +2,12 @@ app = angular.module('gallant.services.glServices', ['ngResource']);
 
 /* istanbul ignore next */
 app.factory('Client', function ($resource) {
-    return $resource('/api/client', {}, {
-        query: {
-            method: 'GET',
-            params: {},
-            isArray: true
-        },
-        retrieve: {
-            method: 'GET',
-            params: {},
-            isArray: true,
-            url: '/api/client/:id'
-        },
+    return $resource('/api/client/:id', {}, {
         fields: {
             method: 'GET',
             url: '/api/client/fields'
         },
-        // the following are untested
-        save: {
-            method: 'POST',
-            params: {
-                client: '@client'
-            },
-        },
-        update: {
-            method: 'PATCH',
-            params: {
-                id: '@id'
-            },
-            url: '/api/client/:id'
-        },
-        replace: {
-            method: 'PUT',
-            params: {
-                id: '@id'
-            },
-            url: '/api/client/:id'
-        }
     });
-
 });
 
 /* istanbul ignore next */
