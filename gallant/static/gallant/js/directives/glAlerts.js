d9bce4c889d76a085631db6f81e172d021206f71
David Garcia <david.agg@gmail.com>
2016-04-19 22:27:26 -0700

    switched alert animations to ng-repeat leave / enter

--- gallant/static/gallant/js/directives/glAlerts.js
+++ gallant/static/gallant/js/directives/glAlerts.js
@@ -3,16 +3,12 @@ app = angular.module('gallant.directives.glAlerts', ['gallant.services.glService
 app.directive('glAlerts', ['glAlertService', function (glAlertService) {
     return {
         restrict: 'A',
-        template: '<uib-alert ng-repeat="alert in alerts.get()" type="{{alert.type}}" class="animate-hide"' +
+        template: '<uib-alert ng-repeat="alert in alerts.get()" type="{{alert.type}}"' +
                   'close="alerts.closeAlertIdx($index)" ng-hide="hideAlert($index)">{{alert.msg}}</uib-alert>',
         controller: function ($scope) {
             $scope.alerts = glAlertService;
         },
         link: function ($scope) {
-            $scope.hideAlert = function ($index) {
-                var len = $scope.alerts.get().length;
-                return len > 3 && (len - $index) > 3;
-            };
         }
     };
 }]);
