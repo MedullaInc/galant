c91f9f56f83ebcedf38e3f4472438335254ef7ce
David Garcia <david.agg@gmail.com>
2016-04-19 22:15:14 -0700

    hide alerts if > 3

--- gallant/static/gallant/js/directives/glAlerts.js
+++ gallant/static/gallant/js/directives/glAlerts.js
@@ -1,9 +1,9 @@
-app = angular.module('gallant.directives.glAlerts', ['gallant.services.glServices', 'ui.bootstrap']);
+app = angular.module('gallant.directives.glAlerts', ['gallant.services.glServices', 'ui.bootstrap', 'ngAnimate']);
 
 app.directive('glAlerts', ['glAlertService', function (glAlertService) {
     return {
         restrict: 'A',
-        template: '<uib-alert ng-repeat="alert in alerts.get()" type="{{alert.type}}"' +
+        template: '<uib-alert ng-repeat="alert in alerts.get()" type="{{alert.type}}" class="animate-hide"' +
                   'close="alerts.closeAlertIdx($index)" ng-hide="hideAlert($index)">{{alert.msg}}</uib-alert>',
         controller: function ($scope) {
             $scope.alerts = glAlertService;
