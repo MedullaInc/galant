9d42ce1825b37e86b98354bc0f7c436811758dde
David Garcia <david.agg@gmail.com>
2016-04-21 17:43:08 -0700

    fix broken test

--- functional_tests/quotes/test_quote_templates.py
+++ functional_tests/quotes/test_quote_templates.py
@@ -148,7 +148,7 @@ class QuoteTemplatesTest(browser.SignedInTest):
         q = get_blank_quote_autofixture(self.user)
         b.get(self.live_server_url + reverse('add_quote_template', kwargs={'quote_id': q.id}))
 
-        browser.wait().until(lambda driver: driver.find_element_by_id('quote_edit')).click()
+        browser.wait().until(lambda driver: driver.find_element_by_id('quote_save').is_displayed)
         self.assertEqual(q.intro().title, {u'en': u'intro'})
 
     def test_add_quote_from_template(self):
