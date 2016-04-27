4492387bce9d11d9375eb90f62f7fa8478dc099d
David Garcia <david.agg@gmail.com>
2016-04-26 18:21:32 -0700

    added min-height to kb col, initialize blank items

--- kanban/static/kanban/js/directives/kbBoardColumn.js
+++ kanban/static/kanban/js/directives/kbBoardColumn.js
@@ -7,10 +7,13 @@ app.directive('kbBoardColumn', function () {
         restrict: 'A',
         replace: true,
         scope: {
-            items: '=',
+            items: '=?',
             title: '@',
         },
-        controller: ['$scope', function ($scope) {}],
+        controller: ['$scope', function ($scope) {
+            if (!$scope.items)
+                $scope.items = [];
+        }],
         templateUrl: '/static/kanban/html/kb_board_column.html',
         link: function ($scope) {
         }
