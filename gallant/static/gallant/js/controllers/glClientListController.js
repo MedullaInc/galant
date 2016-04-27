294ccdb46617309a5e4a27a33b3f442fd5b82c04
David Garcia <david.agg@gmail.com>
2016-04-27 10:03:58 -0700

    sort clients into separate lists for kanban

--- gallant/static/gallant/js/controllers/glClientListController.js
+++ gallant/static/gallant/js/controllers/glClientListController.js
@@ -4,6 +4,11 @@ app.controller('glClientListController', ['$scope', '$http', '$window', 'Client'
     function ($scope, $http, $window, Client) {
         Client.query().$promise.then(function (clients) {
             $scope.clientsSafe = clients;
+            $scope.notQuoted = clients.filter(function (c) { return c.status.indexOf('Pre_Quote') >= 0; });
+            $scope.quoted = clients.filter(function (c) { return c.status.indexOf('Quoted') >= 0; });
+            $scope.projectUnderway = clients.filter(function (c) { return c.status.indexOf('Project_Underway') >= 0; });
+            $scope.pendingPayment = clients.filter(function (c) { return c.status.indexOf('Pending_Payment') >= 0; });
+            $scope.closed = clients.filter(function (c) { return c.status.indexOf('Closed') >= 0; });
         });
 
         Client.fields().$promise.then(function (fields) {
