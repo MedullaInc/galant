b0737a7a2d7cf7218ade43c5b1fffb315f8bf52a
David Garcia <david.agg@gmail.com>
2016-04-28 10:11:39 -0700

    remove console msg

--- kanban/static/kanban/js/directives/kbBoardColumn.js
+++ kanban/static/kanban/js/directives/kbBoardColumn.js
@@ -16,10 +16,8 @@ app.directive('kbBoardColumn', function () {
                 $scope.items = [];
 
             $scope.filterItems = function () {
-                console.log($scope.items);
-                console.log($scope.statusIndex);
                 return $scope.items.filter(function (item) { return +item.status % 10 == +$scope.statusIndex; });
-            }
+            };
         }],
         templateUrl: '/static/kanban/html/kb_board_column.html',
         link: function ($scope) {
