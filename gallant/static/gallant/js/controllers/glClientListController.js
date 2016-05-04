50a26fa17355f8574e225f32ef32fd8aa4d6aee3
David Garcia <david.agg@gmail.com>
2016-05-04 14:52:42 -0700

    added loading spinner to client board

--- gallant/static/gallant/js/controllers/glClientListController.js
+++ gallant/static/gallant/js/controllers/glClientListController.js
@@ -7,16 +7,7 @@ app.controller('glClientListController', ['$scope', '$http', '$window', 'Client'
 
         Client.query().$promise.then(function (clients) {
             $scope.clientsSafe = clients;
-/*            $scope.potential = clients.filter(
-                function (c) { return (+c.status) % 10 == glConstants.ClientStatus.Potential; });
-            $scope.quoted = clients.filter(
-                function (c) { return (+c.status) % 10 == glConstants.ClientStatus.Quoted; });
-            $scope.projectUnderway = clients.filter(
-                function (c) { return (+c.status) % 10 == glConstants.ClientStatus.ProjectUnderway; });
-            $scope.pendingPayment = clients.filter(
-                function (c) { return (+c.status) % 10 == glConstants.ClientStatus.PendingPayment; });
-            $scope.closed = clients.filter(
-                function (c) { return (+c.status) % 10 == glConstants.ClientStatus.Closed; });*/
+            $scope.clientsLoaded = true;
         });
 
         Client.fields().$promise.then(function (fields) {
