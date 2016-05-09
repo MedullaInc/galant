8e741a03186ec11b819f80cce58dd93ce50d6cbe
Gustavo <gustavillo@gmail.com>
2016-05-09 18:04:28 -0500

    working on #338

--- gallant/static/gallant/js/apps/glUserDashboard.js
+++ gallant/static/gallant/js/apps/glUserDashboard.js
@@ -1,6 +1,7 @@
 var app = angular.module('glUserDashboard', [
     'ngResource',
     'ui.bootstrap',
+    'gallant.services.glServices',
     'gallant.directives.glDashboardWorkSummary',
     'gallant.directives.glDashboardMoneySummary',
     'tc.chartjs',
