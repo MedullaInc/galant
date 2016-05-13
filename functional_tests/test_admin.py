b1b6c618e1b3543f42e319596c9efce4ea5a2ca0
David Garcia <david.agg@gmail.com>
2016-05-13 11:43:03 -0700

    refactor admin / login / signup tests add helpers

--- functional_tests/test_admin.py
+++ functional_tests/test_admin.py
@@ -7,9 +7,8 @@ def tearDownModule():
 
 class AdminTest(browser.SignedInTest):
     def test_can_access_admin_site(self):
-        b = browser.instance()
-        b.get(self.live_server_url + '/en/admin/')
+        self.get(self.live_server_url + '/en/admin/')
         # check 'Django administration' heading
-        body = b.find_element_by_tag_name('body')
+        body = self.e_tag('body')
         self.assertIn('Django administration', body.text)
 
--- functional_tests/test_login_signup.py
+++ functional_tests/test_login_signup.py
@@ -8,17 +8,17 @@ def tearDownModule():
 class LoginSignUpTest(browser.SignedInTest):
     def setUp(self):
         super(LoginSignUpTest, self).setUp()
-        browser.instance().get(self.live_server_url+'/en/')
+        self.get(self.live_server_url+'/en/')
 
     def test_cant_login(self):
-        body = browser.instance().find_element_by_tag_name('body')
+        body = self.e_tag('body')
         self.assertNotIn('Account Login', body.text)
 
     def test_cant_sign_up(self):
-        body = browser.instance().find_element_by_tag_name('body')
+        body = self.e_tag('body')
         self.assertNotIn('Sign Up Now!', body.text)
 
     def test_can_sign_out(self):
-        body = browser.instance().find_element_by_tag_name('body')
+        body = self.e_tag('body')
         self.assertIn('Sign Out', body.text)
 
