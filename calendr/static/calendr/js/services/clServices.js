aa7d1c1d727bea4e25b1441f8054cf2d07de5d0b
David Garcia <david.agg@gmail.com>
2016-05-27 15:47:53 -0700

    added missing dependency injection strings for minified versions in calendr module

--- calendr/static/calendr/js/services/clServices.js
+++ calendr/static/calendr/js/services/clServices.js
@@ -1,14 +1,14 @@
 app = angular.module('calendr.services.clServices', ['ngResource']);
 
 /* istanbul ignore next  */
-app.factory('Task', function ($resource) {
+app.factory('Task', ['$resource', function ($resource) {
     return $resource('/en/projects/api/task/:id', {}, {
         update: {
           method: 'PATCH',
         }
     });
 
-});
+}]);
 
 /* istanbul ignore next  */
 app.factory('FC', function () {
