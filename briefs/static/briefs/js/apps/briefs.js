46f821f991599038c711b152696078668e1ba373
David Garcia <david.agg@gmail.com>
2016-05-27 15:13:03 -0700

    added missing dependency injection strings for minified versions

--- briefs/static/briefs/js/apps/briefs.js
+++ briefs/static/briefs/js/apps/briefs.js
@@ -7,11 +7,11 @@ var app = angular.module('brief', [
 ]);
 
 /* istanbul ignore next */
-app.config(function ($httpProvider) {
+app.config(["$httpProvider", function ($httpProvider) {
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-});
+}]);
 
 /* istanbul ignore next */
 app.config(["$resourceProvider", function ($resourceProvider) {
@@ -27,6 +27,6 @@ app.config(["$resourceProvider", function ($resourceProvider) {
 }]);
 
 /* istanbul ignore next */
-app.run(function(editableOptions) {
+app.run(['editableOptions', function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});
+}]);
